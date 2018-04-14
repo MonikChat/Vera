7e56e5639699004bc4d6169a1f2155d45f4b336e
Capuccino <chinodesuuu@gmail.com>
2018-04-14 08:55:05 +0000

    style: fix styling issues and add requirements.txt

--- updater.py
+++ updater.py
@@ -1,19 +1,23 @@
-# Copyright 2018 (c) Clarity
-# This Source is covered by Apache 2.0 License.
-# Please see LICENSE for details
-
+""" Copyright 2018 (c) Clarity
+    This Source is covered by Apache 2.0 License.
+    Please see LICENSE for details
+"""
 import six
 import json
 import aiohttp
 import asyncio
 import async_timeout
-        
-async def request(url: str, query: dict): 
+
+"""  Sends a request to remote API and returns a JSON response
+     Intended for REST APIs for the moment. 
+"""
+ async def request(url: str, query: dict):
     async with async_timeout.timeout(20):
         async with aiohttp.ClientSession() as session:
             async with session.post(url, json=query):
                 return response.text()
+ 
 
-def main():
-    #TODO: finish all query funcs and complete this.
-    print('nya')
+    def main():
+        # TODO: finish all query funcs and complete this.
+        print('nya')
