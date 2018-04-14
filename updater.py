a5ac345b514d79ba1935599f3a3df4b6dc5f96ef
Ayane <enra@headbow.stream>
2018-04-14 08:11:15 +0000

    Initial request() impl

---
+++ updater.py
@@ -0,0 +1,23 @@
+# Copyright 2018 (c) Clarity
+# This Source is covered by Apache 2.0 License.
+# Please see LICENSE for details
+
+import six
+import json
+import aiohttp
+import asyncio
+import async_timeout
+        
+async def request(url, query):
+    async with async_timeout.timeout(20):
+      async with aiohttp.ClientSession() as session:
+         try:
+          json.loads(query)
+         except ValueError:
+          print('query parameter not valid JSON')
+         else:
+          async with session.post(url, query)
+
+def main():
+    #TODO: finish all query funcs and complete this.
+    print('nya')
